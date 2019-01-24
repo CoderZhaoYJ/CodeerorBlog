1e100bdf0ac14fe35472eec78d8a8736a60da306
Codeeror <Codeeror@DESKTOP-8GOJDQP>
2019-01-24 14:38:11 +0800

    添加项目文件。

---
+++ CodeerorBlog/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeerorBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: 首页
+        public ActionResult CodeerorIndex()
+        {
+            return View();
+        }
+    }
+}
